0ddf3d7f859facd7a41a03a4f3fffa8d240ed6f0
logeshrr <logesh.r@xminds.com>
2025-03-14 17:37:32 +0530

    initial commit

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'samp-lens', // Change to your actual GitHub repo name
+  base: 'samp-lens', // Keep your GitHub repo name if needed
+  server: {
+    host: true,  // This exposes the Vite server on your local network
+  },
 });
